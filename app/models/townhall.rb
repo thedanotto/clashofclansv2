d2ac7649d8c7fdb84d0e5b57142205b33caf2093
Dan Otto <theDanOtto@gmail.com>
2015-08-06 17:08:32 -0700

    moved controller code into controller

--- app/models/townhall.rb
+++ app/models/townhall.rb
@@ -3,29 +3,4 @@ class Townhall < ActiveRecord::Base
   has_many :buildings
   after_create :set_buildings
 
-  def new
-    @townhall = Townhall.new()
-  end
-
-  def create
-    @townhall = Townhall.new(townhall_params)
-    @townhall.save
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  private
-
-  def set_buildings
-    BuildingAvailability.all.each do |default_b|
-      Building.create(user_id:current_user.id, townhall_id: current_user.townhall.id, name:default_b.name, level: default_b.default_level, unique_building_code:default_b.unique_building_code)
-    end
-  end
-
 end
