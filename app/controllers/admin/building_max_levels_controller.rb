f66ebdcc27e793ba3d583b528f896582a90587c5
Dan Otto <theDanOtto@gmail.com>
2015-08-08 14:12:04 -0700

    cruded the controller

--- app/controllers/admin/building_max_levels_controller.rb
+++ app/controllers/admin/building_max_levels_controller.rb
@@ -1,8 +1,49 @@
 class Admin::BuildingMaxLevelsController < ApplicationController
   layout "admin"
   load_and_authorize_resource
-  
+  before_action :set_building_max_level, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @building_max_level = BuildingMaxLevel.new
+  end
+
+  def create
+    @building_max_level = BuildingMaxLevel.new(building_max_level_params)
+    if @building_max_level.save
+      redirect_to(admin_building_max_level_path(@building_max_level), notice: "Building max level created")
+    end
+  end
+
   def index
+    @building_max_level = BuildingMaxLevel.all
+  end
+
+  def show
 
   end
+
+  def edit
+
+  end
+
+  def update
+    @building_max_level.update(building_max_level_params)
+    redirect_to(admin_building_max_level_path(@building_max_level), notice: "Building Max Level Updated")
+  end
+
+  def destroy
+    @building_max_level.destroy
+    redirect_to(admin_building_max_levels_path, notice: "Building Max Level DESTROYYYYD")
+  end
+
+
+  private
+  def building_max_level_params
+    params.require(:building_max_levels).permit(:name, :townhall_level, :max_level)
+  end
+
+  def set_building_max_level
+    @building_max_level = BuildingMaxLevel.find(params[:id])
+  end
+
 end
