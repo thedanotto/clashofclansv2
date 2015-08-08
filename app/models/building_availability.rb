cd1a2384785c905205e7c4f4043cdd8e65808034
Dan Otto <theDanOtto@gmail.com>
2015-08-08 14:01:58 -0700

    updated relationship between building basic info

--- app/models/building_availability.rb
+++ app/models/building_availability.rb
@@ -1,4 +1,4 @@
 class BuildingAvailability < ActiveRecord::Base
     has_many :buildings, primary_key: "unique_building_code", foreign_key: "unique_building_code"
-    belongs_to :basic_info, primary_key: "name", foreign_key: "name"
+    belongs_to :building_basic_info, primary_key: "name", foreign_key: "name"
 end
