b8c3cae832c8df4369087deb774dd7aa5c7af438
Dan Otto <theDanOtto@gmail.com>
2015-08-08 14:00:23 -0700

    added set_cumulative_cost

--- app/models/building_cost_info.rb
+++ app/models/building_cost_info.rb
@@ -1,4 +1,15 @@
 class BuildingCostInfo < ActiveRecord::Base
   self.primary_keys = :name, :level
   has_many :buildings, foreign_key: [:name, :level]
+  before_save :set_cumulative_cost
+
+  private
+  def set_cumulative_cost
+    cumulative_cost = BuildingCostInfo.where('name = ? AND level < ?', self.name, self.level).sum(:cost)
+    cumulative_time = BuildingCostInfo.where('name = ? AND level < ?', self.name, self.level).sum(:time)
+    self.cumulative_cost = self.cost + cumulative_cost
+    self.cumulative_time = self.time + cumulative_time
+  end
+  # we need to update cumulative cost and things when we create, update, and whatnot
+
 end
