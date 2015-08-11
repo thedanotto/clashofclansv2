fbad98eb7f2f4551f2c11491e448511e390db9fc
Dan Otto <theDanOtto@gmail.com>
2015-08-11 10:45:19 -0700

    added image url method

--- app/models/building_cost_info.rb
+++ app/models/building_cost_info.rb
@@ -3,6 +3,10 @@ class BuildingCostInfo < ActiveRecord::Base
   has_many :buildings, foreign_key: [:name, :level]
   before_save :set_cumulative_cost
 
+  def image_url
+    "/assets/#{self.name}#{self.level}.png"
+  end
+
   private
   def set_cumulative_cost
     cumulative_cost = BuildingCostInfo.where('name = ? AND level < ?', self.name, self.level).sum(:cost)
@@ -11,5 +15,4 @@ class BuildingCostInfo < ActiveRecord::Base
     self.cumulative_time = self.time + cumulative_time
   end
   # we need to update cumulative cost and things when we create, update, and whatnot
-
 end
