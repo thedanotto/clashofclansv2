fc59097a7385ee39da100647a1ed206a2dd6f0e3
Dan Otto <theDanOtto@gmail.com>
2015-08-16 13:26:39 -0600

    added methods to wall

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -3,5 +3,35 @@ class Wall < ActiveRecord::Base
   belongs_to :townhall
   belongs_to :building_cost_info, class_name:'BuildingCostInfo', foreign_key: [:name, :level]
 
-  
+
+
+  def max_wall_level
+    BuildingMaxLevel.find_by(townhall_level: self.townhall.level, name: name).max_level
+  end
+
+  def max_wall_count
+    BuildingMaxLevel.find_by(townhall_level: self.townhall.level, name: name).max_count
+  end
+
+  class << self
+    def max_wall_level
+      BuildingMaxLevel.find_by(townhall_level: self.first.townhall.level, name: name.downcase).max_level
+    end
+
+    def max_wall_count
+      BuildingMaxLevel.find_by(townhall_level: self.first.townhall.level, name: name.downcase).max_count
+    end
+
+    def town_total_walls
+      self.sum(:wall_count)
+    end
+
+    def remaining_walls_to_dispurse
+      max_wall_count - town_total_walls
+    end
+
+    def cumulative_cost
+      self.joins(:building_cost_info).sum("wall_count * cumulative_cost")
+    end
+  end
 end
