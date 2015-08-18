c4d0bff4def221a14420810e0ee2c7ba42576453
Dan Otto <theDanOtto@gmail.com>
2015-08-17 20:20:30 -0600

    added methods for max wall

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -3,8 +3,6 @@ class Wall < ActiveRecord::Base
   belongs_to :townhall
   belongs_to :building_cost_info, class_name:'BuildingCostInfo', foreign_key: [:name, :level]
 
-
-
   def max_wall_level
     BuildingMaxLevel.find_by(townhall_level: self.townhall.level, name: name).max_level
   end
@@ -14,6 +12,14 @@ class Wall < ActiveRecord::Base
   end
 
   class << self
+    def cumulative_cost_by_wall_level(wall_level)
+      BuildingCostInfo.find_by(level: wall_level, name:"wall").cumulative_cost
+    end
+
+    def th_maxed_wall_cost
+      self.max_wall_count * self.cumulative_cost_by_wall_level(self.max_wall_level)
+    end
+
     def max_wall_level
       BuildingMaxLevel.find_by(townhall_level: self.first.townhall.level, name: name.downcase).max_level
     end
