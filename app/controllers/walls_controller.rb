2bde33137a19cee72085d44f3da6efc9c6597ff4
Dan Otto <theDanOtto@gmail.com>
2015-08-19 08:56:35 -0600

    added walls form and controller update

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -2,10 +2,15 @@ class WallsController < ApplicationController
   layout "admin_sidenav"
 
   def index
-
+    @walls = current_user.walls.order_by_level
   end
 
   def update
-
+    # updates all walls
+    @walls = current_user.walls
+    @walls.each do |wall|
+      wall.update_attributes(wall_count:params["wall"][wall.level.to_s])
+    end
+    redirect_to(walls_path, notice:"Walls Updated")
   end
 end
