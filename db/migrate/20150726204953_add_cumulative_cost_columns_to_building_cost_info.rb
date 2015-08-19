2f6252b346073ebdcbee4fd198f69606f99ee54d
Dan Otto <theDanOtto@gmail.com>
2015-08-19 15:17:20 -0600

    updated bigger limit to migration file

--- db/migrate/20150726204953_add_cumulative_cost_columns_to_building_cost_info.rb
+++ db/migrate/20150726204953_add_cumulative_cost_columns_to_building_cost_info.rb
@@ -1,6 +1,6 @@
 class AddCumulativeCostColumnsToBuildingCostInfo < ActiveRecord::Migration
   def change
-    add_column :building_cost_infos, :cumulative_cost, :integer
-    add_column :building_cost_infos, :cumulative_time, :integer
+    add_column :building_cost_infos, :cumulative_cost, :integer, :limit => 8
+    add_column :building_cost_infos, :cumulative_time, :integer, :limit => 8
   end
 end
