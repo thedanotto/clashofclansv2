22e92c27b14e89a7e9fb46fa6fe30f485caf485d
Dan Otto <theDanOtto@gmail.com>
2015-08-19 09:34:16 -0600

    added active scope to walls methods

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -2,12 +2,12 @@ class WallsController < ApplicationController
   layout "admin_sidenav"
 
   def index
-    @walls = current_user.walls.order_by_level
+    @walls = current_user.walls.active(current_user.townhall.level).order_by_level
   end
 
   def update
     # updates all walls
-    @walls = current_user.walls
+    @walls = current_user.walls.active(current_user.townhall.level)
     @walls.each do |wall|
       wall.update_attributes(wall_count:params["wall"][wall.level.to_s])
     end
