9b1be9f768b3e8763cffd0eed4578b2b3cd6a22d
Dan Otto <theDanOtto@gmail.com>
2015-08-19 22:15:52 -0600

    added validation to builder

--- app/models/builder.rb
+++ app/models/builder.rb
@@ -1,5 +1,7 @@
 class Builder < ActiveRecord::Base
   belongs_to :user
+  validates :b_count, presence: true
+  validates_inclusion_of :b_count, in: ->(builder) { 1..builder.max_builders }, message: "Enter a number 1 through 5"
 
   def max_builders
     return 5
